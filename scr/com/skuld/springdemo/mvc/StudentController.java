7f2e4096cee1153ec231d90a6f98d797e0733e50
zülfü balkan <zulfubalkan@hotmail.com>
2022-08-02 13:44:05 +0300

    dataBinder with initBinder

--- scr/com/skuld/springdemo/mvc/StudentController.java
+++ scr/com/skuld/springdemo/mvc/StudentController.java
@@ -3,9 +3,12 @@ package com.skuld.springdemo.mvc;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,6 +16,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/student")
 public class StudentController {
 
+	//initBinder for trim whitespaces to null
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor stringTrimmer = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, stringTrimmer);
+	}
+	
 	//Method for Display Form 
 	@RequestMapping("/showForm")
 	public String showForm(Model model) {
--- scr/com/skuld/springdemo/mvc/TeacherController.java
+++ scr/com/skuld/springdemo/mvc/TeacherController.java
@@ -2,9 +2,12 @@ package com.skuld.springdemo.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,6 +15,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/teacher")
 public class TeacherController {
 
+	//add an initBinder -Trim whitespace string to Null
+	@InitBinder
+	public void initBinder(WebDataBinder databinder) {
+		
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);		
+		databinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
+	
 	@RequestMapping("/showForm")
 	public String showForm(Model model) {
 		//Create Teacher object
